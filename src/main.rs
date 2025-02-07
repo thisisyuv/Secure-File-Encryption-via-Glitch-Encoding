bcc05f4e2be00ce0454099697ccc9f37878bc281
thisisyuv <141017308+thisisyuv@users.noreply.github.com>
2025-02-07 11:53:19 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -14,15 +14,12 @@ use crate::args::Arguments;
 //Make it calculate how much data is jammed in 1 frame for user
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    println!("Welcome to ISG (Infinite Storage Glitch)");
-    println!("This tool allows you to turn any file into a compression-resistant video that can be uploaded to YouTube for Infinite Storage:tm:");
+    println!("Welcome to SFE (Secure File Encryption)");
+    println!("This tool allows you to encrypt any file into a compression-resistant video.");
     println!("\nHow to use:");
     println!("1. Zip all the files you will be uploading");
     println!("2. Use the embed option on the archive (THE VIDEO WILL BE SEVERAL TIMES LARGER THAN THE FILE, 4x in case of optimal compression resistance preset)");
-    println!(
-        "3. Upload the video to your YouTube channel. You probably want to keep it up as unlisted"
-    );
-    println!("4. Use the download option to get the video back");
+    println!("3. Use the download option to get the video from Youtube");
     println!("5. Use the dislodge option to get your files back from the downloaded video");
     println!("6. PROFIT\n");
 
